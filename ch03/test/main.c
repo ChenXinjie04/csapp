f010383721acc3b9fea227cf0e5f455c5c9e3ffc
Chen Xinjie <xinjiechen04@gmail.com>
2025-03-03 10:57:40 +0800

    ch03 52

--- ch03/test/main.c
+++ ch03/test/main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
-struct P1 {int i; char c; int j; char d; };
-struct P2 {int i; char c; char d; long j; };
-
 int main() {
-	printf("The size of struct P1 is %lu\n", sizeof(struct P1));
-	printf("The size of struct P2 is %lu\n", sizeof(struct P2));
+	long local;
+	printf("local at %p\n", &local);
 	return 0;
 }
