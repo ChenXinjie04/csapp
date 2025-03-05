6780054966675aeffcdfc6d6cf453839f75fa5e0
Chen Xinjie <xinjiechen04@gmail.com>
2025-03-05 17:13:58 +0800

    ch03 73

--- ch03/73/main.c
+++ ch03/73/main.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <math.h>
 
 typedef enum {NEG, ZERO, POS, OTHER} range_t;
 
@@ -7,25 +8,45 @@ range_t find_range(float x) {
 	asm(
 		"vxorps %%xmm1, %%xmm1, %%xmm1 \n\t"
 		"vucomiss %%xmm1, %[x]\n\t"
+		"jp .p \n\t"
 		"ja .a \n\t"
 		"jb .b \n\t"
 		"je .e \n\t"
-		"jp .p \n\t"
 		".a: \n\t"
 		"movl $2, %[r] \n\t"
+		"jmp .done \n\t"
 		".b: \n\t"
 		"movl $0, %[r] \n\t"
+		"jmp .done \n\t"
 		".e: \n\t"
 		"movl $1, %[r] \n\t"
+		"jmp .done \n\t"
 		".p: \n\t"
-		"movl $3, %[r]"
+		"movl $3, %[r] \n\t"
+		".done:"
 		: [r] "=r" (result)
 		: [x] "x" (x)
 	);
 	return result;
 }
 
+void test() {
+	range_t answer;
+	for (double i = -10; i < 10; i += 0.1) {
+		if (i < 0) {
+			answer = NEG;
+		} else if (i == 0) {
+			answer = ZERO;
+		} else {
+			answer = POS;
+		}
+		assert(find_range(i) == answer);
+	}
+	double i = NAN;
+	assert(find_range(i) == OTHER);
+}
+
 int main() {
-	assert(0 == find_range(0.0));
+	test();	
 	return 0;
 }
