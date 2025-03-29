96d2ad7b879523aad80f28ef1d972066dd5fb035
Chenxinjie <xinjiechen04@gmail.com>
2025-03-29 16:08:54 +0800

    ch11 3

--- ch11/3/main.c
+++ ch11/3/main.c
@@ -1,11 +1,22 @@
-#include <stdio.h>
 #include <arpa/inet.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
 
 int main(int argc, char *argv[]) {
-	in_
-	if (argc != 2) {
-		printf("usage: dd2hext <dotted decimal ipv4 address>\n");
-	}
+  struct in_addr in_addr;
+  uint32_t addr;
+  if (argc != 2) {
+    printf("usage: dd2hext <dotted decimal ipv4 address>\n");
+  }
 
-	return 0;
+  if (!inet_pton(AF_INET, argv[1], &in_addr)) {
+    printf("inet_pton\n");
+    exit(1);
+  }
+  addr = ntohl(in_addr.s_addr);
+
+  printf("0x%x\n", addr);
+  return 0;
 }
