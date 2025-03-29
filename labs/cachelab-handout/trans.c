c13809b03ac5cefa5e0eb44b9a42560a40e0c9b2
chenxinjie <xinjiechen04@gmail.com>
2025-03-29 09:36:04 +0800

    matrix 64

--- labs/cachelab-handout/trans.c
+++ labs/cachelab-handout/trans.c
@@ -22,6 +22,103 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int a, b, c, d, e, f, g, h;
+    if (M == 32) {
+        for (int i = 0; i < 32; i += 8) {
+            for (int j = 0; j < 32; j += 8) {
+                for (int k = 0; k < 8; ++k) {
+                    a = A[i+k][j+0];
+                    b = A[i+k][j+1];
+                    c = A[i+k][j+2];
+                    d = A[i+k][j+3];
+                    e = A[i+k][j+4];
+                    f = A[i+k][j+5];
+                    g = A[i+k][j+6];
+                    h = A[i+k][j+7];
+                    B[j+k][i+0] = a;
+                    B[j+k][i+1] = b;
+                    B[j+k][i+2] = c;
+                    B[j+k][i+3] = d;
+                    B[j+k][i+4] = e;
+                    B[j+k][i+5] = f;
+                    B[j+k][i+6] = g;
+                    B[j+k][i+7] = h;
+                }
+                for (int k = 0; k < 8; ++k) {
+                    for (int l = 0; l < k; ++l) {
+                        a = B[j+k][i+l];
+                        B[j+k][i+l] = B[j+l][i+k];
+                        B[j+l][i+k] = a;
+                    }
+                }
+            }
+        }
+    }
+    if (M == 64) {
+        for (int i = 0; i < 64; i += 8) {
+            for (int j = 0; j < 64; j += 8) {
+                for (int k = 0; k < 4; ++k) {
+                    a = A[i+k][j+0];
+                    b = A[i+k][j+1];
+                    c = A[i+k][j+2];
+                    d = A[i+k][j+3];
+                    e = A[i+k][j+4];
+                    f = A[i+k][j+5];
+                    g = A[i+k][j+6];
+                    h = A[i+k][j+7];
+                    B[j+0][i+k] = a;
+                    B[j+1][i+k] = b;
+                    B[j+2][i+k] = c;
+                    B[j+3][i+k] = d;
+                    B[j+0][i+k+4] = e;
+                    B[j+1][i+k+4] = f;
+                    B[j+2][i+k+4] = g;
+                    B[j+3][i+k+4] = h;
+                }
+                for (int k = 0; k < 4; ++k) {
+                    a = A[i+4][j+k];
+                    b = A[i+5][j+k];
+                    c = A[i+6][j+k];
+                    d = A[i+7][j+k];
+
+                    e = B[j+k][i+4];
+                    f = B[j+k][i+5];
+                    g = B[j+k][i+6];
+                    h = B[j+k][i+7];
+
+                    B[j+k][i+4] = a;
+                    B[j+k][i+5] = b;
+                    B[j+k][i+6] = c;
+                    B[j+k][i+7] = d;
+
+                    B[j+k+4][i] = e;
+                    B[j+k+4][i+1] = f;
+                    B[j+k+4][i+2] = g;
+                    B[j+k+4][i+3] = h;
+                }
+
+                for (int k = 4; k < 8; ++k) {
+                    a = A[i+k][j+4];
+                    b = A[i+k][j+5];
+                    c = A[i+k][j+6];
+                    d = A[i+k][j+7];
+
+                    B[j+k][i+4] = a;
+                    B[j+k][i+5] = b;
+                    B[j+k][i+6] = c;
+                    B[j+k][i+7] = d;
+                }
+
+                for (int k = 4; k < 8; ++k) {
+                    for (int l = 4; l < k; ++l) {
+                        a = B[j+k][i+l];
+                        B[j+k][i+l] = B[j+l][i+k];
+                        B[j+l][i+k] = a;
+                    }
+                }
+            }
+        }
+    }
 }
 
 /* 
