b8479482259d686da614009ada8d3b671e4829d0
Chen Xinjie <xinjiechen04@gmail.com>
2025-04-11 19:38:45 +0800

    done?

--- ch12/tiny-pre/test.sh
+++ ch12/tiny-pre/test.sh
@@ -1,5 +1,5 @@
-CONCURRENT_LEVEL=5
-NREQUEST=5000
+CONCURRENT_LEVEL=10
+NREQUEST=20000
 
 make clean
 make
@@ -8,8 +8,5 @@ TINY_PID=$!
 ab -n ${NREQUEST} -c ${CONCURRENT_LEVEL} localhost:8000/ &
 apache_pid=$!
 sleep 5
-code out.log
-jobs
-echo ${TINY_PID}
 kill -9 ${TINY_PID}
 kill -9 ${apache_pid}
